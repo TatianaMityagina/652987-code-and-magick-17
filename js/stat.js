4d8b3992989831b5c02950ca75e2a210d3577a07
TatianaMityagina <394999325@mail.ru>
2019-06-08 20:36:50 +0300

    Задание 3 внесла поправки

--- js/stat.js
+++ js/stat.js
@@ -1,3 +1,4 @@
+'use strict';
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 
@@ -7,17 +8,17 @@ var renderRect = function (ctx, x, y, color) {
 };
 
 window.renderStatistics = function (ctx, names, times) {
-  //Shadow rendering
+  // Shadow rendering
   renderRect(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
 
-  //Cloud rendering
+  // Cloud rendering
   renderRect(ctx, 100, 10, '#fff');
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
   ctx.fillText('Ура вы победили!', 120, 40);
   ctx.fillText('Список результатов:', 120, 60);
 
-  //Find the maximum element
+  // Find the maximum element
   var maxTime = times[0];
   for (var i = 1; i < times.length; i++) {
     if (times[i] > maxTime) {
@@ -25,7 +26,7 @@ window.renderStatistics = function (ctx, names, times) {
     }
   }
 
-  //Histogram rendering
+  // Histogram rendering
   var HISTOGRAM_HEIGHT = 150;
   var WIDTH_COLUMN = 40;
   var INDENT = 50;
@@ -35,15 +36,15 @@ window.renderStatistics = function (ctx, names, times) {
 
   for (var i = 0; i < names.length; i++) {
 
-    //Find percentage of number
+    // Find percentage of number
     var time = Math.floor(times[i]);
     var percent = Math.floor((time * 100) / Math.floor(maxTime));
 
-    //Find the height of the column
+    // Find the height of the column
     var heightColumn = (HISTOGRAM_HEIGHT * percent) / 100;
     var currentPadding = PADDING_TOP_COLUMNS + (PADDING_TOP_COLUMNS - heightColumn);
 
-    //Column rendering
+    // Column rendering
     if (names[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
@@ -52,11 +53,11 @@ window.renderStatistics = function (ctx, names, times) {
 
     ctx.fillRect(leftColumn, currentPadding, WIDTH_COLUMN, heightColumn);
 
-    //Text time rendering
+    // Text time rendering
     ctx.fillStyle = '#000';
     ctx.fillText(time, leftColumn, currentPadding - 10);
 
-    //Text name rendering
+    // Text name rendering
     ctx.fillText(names[i], leftColumn, PADDING_TOP_TEXT_NAMES);
 
     leftColumn += WIDTH_COLUMN + INDENT;
