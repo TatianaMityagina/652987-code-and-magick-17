04d065ae9a6c10831547c476fb03f2f8fe58b7af
TatianaMityagina <394999325@mail.ru>
2019-06-24 15:07:02 +0300

    Задание 11 Инкапсулировала код

--- js/dialog.js
+++ js/dialog.js
@@ -1,8 +1,104 @@
 'use strict';
 (function () {
 
+  var ESC_KEYCODE = 27;
+  var ENTER_KEYCODE = 13;
+
   var setup = document.querySelector('.setup');
   var dialogHandler = setup.querySelector('.upload');
+  var setupOpen = document.querySelector('.setup-open');
+  var setupClose = setup.querySelector('.setup-close');
+  var setupUserName = setup.querySelector('.setup-user-name');
+  var setupWizardCoat = setup.querySelector('.wizard-coat');
+  var setupWizardEyes = setup.querySelector('.wizard-eyes');
+  var setupFireball = setup.querySelector('.setup-fireball-wrap');
+  var setupUserNameFocus = false;
+  var initialCoords = {
+    x: getComputedStyle(setup).left,
+    y: getComputedStyle(setup).top
+  };
+
+  // Окно закроется только в случае если оба условия true
+  var onPopupEscPress = function (evt) {
+    if (!setupUserNameFocus && evt.keyCode === ESC_KEYCODE) {
+      closePopup();
+    }
+  };
+
+  // Определяем функцию при которой имя пользователя в фокусе
+  var onSetupUserNameFocus = function () {
+    setupUserNameFocus = true;
+  };
+
+  // Определяем функцию при которой имя пользователя не в фокусе
+  var onSetupUserNameFocusout = function () {
+    setupUserNameFocus = false;
+  };
+
+  // При неправильном заполнении имени высвечивается сообщение след. вида
+  setupUserName.addEventListener('invalid', function (evt) {
+    if (setupUserName.validity.tooShort) {
+      setupUserName.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+    } else if (setupUserName.validity.tooLong) {
+      setupUserName.setCustomValidity('Имя не должно превышать 25-ти символов');
+    } else if (setupUserName.validity.valueMissing) {
+      setupUserName.setCustomValidity('Обязательное поле');
+    } else {
+      setupUserName.setCustomValidity('');
+    }
+  });
+
+  // Сбросить значение поля, если это значение стало корректно
+  setupUserName.addEventListener('input', function (evt) {
+    var target = evt.target;
+    if (target.value.length < 2) {
+      target.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+    } else {
+      target.setCustomValidity('');
+    }
+  });
+
+  var openPopup = function () {
+    setup.classList.remove('hidden');
+    document.addEventListener('keydown', onPopupEscPress);
+    setupUserName.addEventListener('focus', onSetupUserNameFocus);
+    setupUserName.addEventListener('focusout', onSetupUserNameFocusout);
+    setupWizardCoat.addEventListener('click', window.onSetupWizardCoatClick);
+    setupWizardEyes.addEventListener('click', window.onSetupWizardEyesClick);
+    setupFireball.addEventListener('click', window.onSetupFireballClick);
+  };
+
+  var closePopup = function () {
+    setup.classList.add('hidden');
+    setup.style.top = initialCoords.y;
+    setup.style.left = initialCoords.x;
+    document.removeEventListener('keydown', onPopupEscPress);
+    setupUserName.removeEventListener('focus', onSetupUserNameFocus);
+    setupUserName.removeEventListener('focusout', onSetupUserNameFocusout);
+    setupWizardCoat.removeEventListener('click', window.onSetupWizardCoatClick);
+    setupWizardEyes.removeEventListener('click', window.onSetupWizardEyesClick);
+    setupFireball.removeEventListener('click', window.onSetupFireballClick);
+  };
+
+  setupOpen.addEventListener('click', function () {
+    openPopup();
+  });
+
+  setupOpen.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === ENTER_KEYCODE) {
+      openPopup();
+    }
+  });
+
+  setupClose.addEventListener('click', function () {
+    closePopup();
+  });
+
+  setupClose.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === ENTER_KEYCODE) {
+      closePopup();
+    }
+  });
 
   dialogHandler.addEventListener('mousedown', function (downEvt) {
     downEvt.preventDefault();
